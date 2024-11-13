d93483b961d4fe3561381f6ee4ac1eed3c7fd069
Mateus Moura <mateusvcsmoura@gmail.com>
2024-11-13 18:39:19 -0300

    Upload de Aula 03 - Módulo Javascript II

---
+++ Fundamentos (FullStack)/4 - Módulo Javascript II/Aulas/aula003/index.js
@@ -0,0 +1,8 @@
+const person = {};
+
+person.name = "Mateus";
+person.age = 18;
+person.friends = ["Javascript", "Python", "League of Legends"];
+person.address = { street: 'Vicente do Rego Monteiro', number: 100, city: 'São Paulo' };
+
+console.log(person);
