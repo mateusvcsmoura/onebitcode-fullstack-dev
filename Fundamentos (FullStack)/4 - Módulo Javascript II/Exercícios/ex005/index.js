a998262c02a7c191ba6fc84a664063c141392d5c
Mateus Moura <mateusvcsmoura@gmail.com>
2024-12-06 20:56:07 -0300

    Fixed: Melhor estrutura de código, uso de funções para cada switch case.

--- Fundamentos (FullStack)/4 - Módulo Javascript II/Exercícios/ex005/index.js
+++ Fundamentos (FullStack)/4 - Módulo Javascript II/Exercícios/ex005/index.js
@@ -3,6 +3,9 @@ let jobs = Array(
 
     { 'job-name': "QA Assurance", 'description': "Responsável pela qualidade dos códigos.", 'limit-date': "31/12/2024", 'candidates': ["Douglas", "Ellen"] }
 );
+let flag = true;
+
+// Functions
 
 function translate(word) {
     let words = {
@@ -15,8 +18,86 @@ function translate(word) {
     return words[word];
 }
 
-let flag = true;
+function listAllJobs() {
+    let jobsString = jobs.reduce(function (accumulator, job) {
+        return accumulator + `Índice: ${jobs.indexOf(job)} | ${job["job-name"]}: ${job.candidates.length} Inscritos.\n`;
+    }, "");
 
+    window.alert(`Vagas disponíveis\n\n${jobsString}`);
+}
+
+function createNewJob() {
+    let newJob = {};
+
+    let jobName = window.prompt("Nome da Vaga");
+    let jobDescription = window.prompt("Descrição da Vaga");
+    let jobLimitDate = window.prompt("Data Limite (DD/MM/YYYY)");
+
+    let confirmation = window.confirm(`Deseja confirmar a vaga abaixo?\n\nNome da Vaga: ${jobName}\nDescrição: ${jobDescription}\nData limite: ${jobLimitDate}`);
+
+    if (confirmation) {
+        newJob["job-name"] = jobName;
+        newJob["description"] = jobDescription;
+        newJob["limit-date"] = jobLimitDate;
+        newJob["candidates"] = Array();
+
+        jobs.push(newJob);
+
+        window.alert("Upload de Vaga CONCLUÍDO COM SUCESSO.");
+    } else {
+        window.alert("Voltando ao menu.");
+    }
+}
+
+function viewJob() {
+    let jobIndex = window.parseFloat(window.prompt("Visualizar Vaga\n\nInsira o índice da vaga"));
+    let jobsString = "";
+
+    for (const [key, value] of Object.entries(jobs[jobIndex])) {
+        jobsString += `${translate(key)}:  ${value}\n`;
+    }
+
+    window.alert(`Vaga\n\n${jobsString}`);
+}
+
+function applyCandidate() {
+    let candidateName = window.prompt("Inserir Candidato\n\nNome");
+    let jobIndex = window.parseFloat(window.prompt("Inserir Candidato\n\nInsira o índice da vaga"));
+    let jobsString = "";
+
+    for (const [key, value] of Object.entries(jobs[jobIndex])) {
+        jobsString += `${translate(key)}:  ${value}\n`;
+    }
+
+    let candidateConfirmation = window.confirm(`Confirma inserir ${candidateName} na Vaga\n\n${jobsString}`);
+
+    if (candidateConfirmation) {
+        jobs[jobIndex].candidates.push(candidateName);
+    } else {
+        window.alert("Voltando ao menu.");
+    }
+
+}
+
+function deleteJob() {
+    let jobIndex = window.parseFloat(window.prompt("Inserir Candidato\n\nInsira o índice da vaga"));
+    let jobsString = "";
+
+    for (const [key, value] of Object.entries(jobs[jobIndex])) {
+        jobsString += `${translate(key)}:  ${value}\n`;
+    }
+
+    let excludeConfirmation = window.confirm(`Confirma deletar a Vaga\n\n${jobsString}`);
+
+    if (excludeConfirmation) {
+        jobs.splice(jobIndex, 1);
+        window.alert("Vaga excluída com sucesso.");
+    } else {
+        window.alert("Voltando ao menu.");
+    }
+}
+
+// Loop
 
 do {
     let menu = `LinkedIn\n\nVagas: ${jobs.length}\n\n1 - Listar Vagas\n2 - Criar Vaga\n3 - Visualizar Vaga\n4 - Inscrever Candidato\n5 - Excluir Vaga\n6 - Sair\n\nOpção`;
@@ -24,80 +105,19 @@ do {
 
     switch (opt) {
         case 1:
-            let jobsString = jobs.reduce(function (accumulator, job) {
-                return accumulator + `Índice: ${jobs.indexOf(job)} | ${job["job-name"]}: ${job.candidates.length} Inscritos.\n`;
-            }, "");
-
-            window.alert(`Vagas disponíveis\n\n${jobsString}`);
+            listAllJobs();
             break;
         case 2:
-            let newJob = {};
-
-            let jobName = window.prompt("Nome da Vaga");
-            let jobDescription = window.prompt("Descrição da Vaga");
-            let jobLimitDate = window.prompt("Data Limite (DD/MM/YYYY)");
-
-            let confirmation = window.confirm(`Deseja confirmar a vaga abaixo?\n\nNome da Vaga: ${jobName}\nDescrição: ${jobDescription}\nData limite: ${jobLimitDate}`);
-
-            if (confirmation) {
-                newJob["job-name"] = jobName;
-                newJob["description"] = jobDescription;
-                newJob["limit-date"] = jobLimitDate;
-                newJob["candidates"] = Array();
-
-                jobs.push(newJob);
-
-                window.alert("Upload de Vaga CONCLUÍDO COM SUCESSO.");
-            } else {
-                window.alert("Voltando ao menu.");
-            }
+            createNewJob();
             break;
         case 3:
-            let jobIndex = window.parseFloat(window.prompt("Visualizar Vaga\n\nInsira o índice da vaga"));
-            let jobViewString = "";
-
-            for (const [key, value] of Object.entries(jobs[jobIndex])) {
-                jobViewString += `${translate(key)}:  ${value}\n`;
-            }
-
-            window.alert(`Vaga\n\n${jobViewString}`);
-
+            viewJob();
             break;
         case 4:
-            let candidateName = window.prompt("Inserir Candidato\n\nNome");
-            let jobIndex1 = window.parseFloat(window.prompt("Inserir Candidato\n\nInsira o índice da vaga"));
-            let jobString = "";
-
-            for (const [key, value] of Object.entries(jobs[jobIndex1])) {
-                jobString += `${translate(key)}:  ${value}\n`;
-            }
-
-            let candidateConfirmation = window.confirm(`Confirma inserir ${candidateName} na Vaga\n\n${jobString}`);
-
-            if (candidateConfirmation) {
-                jobs[jobIndex1].candidates.push(candidateName);
-            } else {
-                window.alert("Voltando ao menu.");
-            }
-
+            applyCandidate();
             break;
         case 5:
-            let jobIndex2 = window.parseFloat(window.prompt("Inserir Candidato\n\nInsira o índice da vaga"));
-            let jobStringExclude = "";
-
-            for (const [key, value] of Object.entries(jobs[jobIndex2])) {
-                jobStringExclude += `${translate(key)}:  ${value}\n`;
-            }
-
-            let excludeConfirmation = window.confirm(`Confirma deletar a Vaga\n\n${jobStringExclude}`);
-
-            if (excludeConfirmation) {
-                jobs.splice(jobIndex2, 1);
-                window.alert("Vaga excluída com sucesso.");
-            } else {
-                window.alert("Voltando ao menu.");
-            }
-
+            deleteJob();
             break;
         case 6:
             flag = false;
