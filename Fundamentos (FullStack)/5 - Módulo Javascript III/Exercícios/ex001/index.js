abf4048967a6432519b8549fdf092619e54aceb4
Mateus Moura <mateusvcsmoura@gmail.com>
2024-12-12 20:40:30 -0300

    Fixed: Melhor estrutura de DOM

--- Fundamentos (FullStack)/5 - Módulo Javascript III/Exercícios/ex001/index.js
+++ Fundamentos (FullStack)/5 - Módulo Javascript III/Exercícios/ex001/index.js
@@ -6,40 +6,13 @@ function addPlayer() {
     const confirmation = window.confirm(`Deseja escalar ${playerName.value}?\n\nPosição: ${playerPosition.value}\nNúmero: ${playerNumber.value}`);
 
     if (confirmation) {
-        const playerList = document.querySelector('section#players-list');
+        const playerList = document.querySelector('ul#players-list');
+        const player = document.createElement('li');
+        player.id = playerNumber.value;
 
-        const ul = document.createElement('ul');
-        const h3 = document.createElement('h3');
-        h3.innerText = `Jogador ${playerNumber.value}`;
-        ul.appendChild(h3);
+        player.innerText = `${playerPosition.value}: ${playerName.value} | Camisa ${playerNumber.value}`;
 
-        const nameLi = document.createElement('li');
-        nameLi.innerText = "Nome do Jogador: ";
-        const nameSpan = document.createElement('span');
-        nameSpan.innerText = playerName.value;
-
-        nameLi.appendChild(nameSpan);
-        ul.appendChild(nameLi);
-
-        const positionLi = document.createElement('li');
-        positionLi.innerText = "Posição: ";
-        const positionSpan = document.createElement('span');
-        positionSpan.innerText = playerPosition.value;
-
-        positionLi.appendChild(positionSpan);
-        ul.appendChild(positionLi);
-
-        const numberLi = document.createElement('li');
-        numberLi.innerText = "Número: ";
-        const numberSpan = document.createElement('span');
-        numberSpan.innerText = playerNumber.value;
-
-        numberLi.appendChild(numberSpan);
-        ul.appendChild(numberLi);
-
-        // PASSANDO O ID PARA FICAR FACIL DE ENCONTRAR A UL NA SECTION
-        ul.id = playerNumber.value;
-        playerList.append(ul);
+        playerList.appendChild(player);
     }
 
     playerPosition.value = "";
@@ -54,7 +27,7 @@ function removePlayer() {
     // BUSCANDO O ELEMENTO QUE TEM O ID DA CAMISA
     const playerItem = document.getElementById(playerNumberToRemove.value);
 
-    const confirmation = window.confirm(`Deseja remover o camisa ${playerNumberToRemove.value}?`)
+    const confirmation = window.confirm(`Deseja remover o ${playerItem.innerText}?`)
 
     if (confirmation) {
         players.removeChild(playerItem);
