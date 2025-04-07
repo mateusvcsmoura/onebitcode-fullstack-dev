30562c3fe34a5bca34fb04f64545287e13f58d75
Mateus Moura <mateusvcsmoura@gmail.com>
2025-04-07 16:05:54 -0300

    Upload de Aula 009 - Módulo Javascript IV, Adicionando um .gitignore para ignorar os módulos instalados.

---
+++ Fundamentos (FullStack)/6 - Módulo Javascript IV/Aulas/aula009/index.js
@@ -0,0 +1,12 @@
+const lodash = require("lodash");
+
+const a = lodash.toNumber("14");
+console.log(a);
+console.log(typeof a);
+
+const b = lodash.isArray(a);
+console.log(b);
+
+const c = lodash.kebabCase("Mateus Moura");
+console.log(c);
+
