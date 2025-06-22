279f5f977a8b72ed629dc93596e285b93e3a7fce
Mateus Moura <mateusvcsmoura@gmail.com>
2025-06-21 23:22:13 -0300

    Implementação de melhoria no Exercício 002 | Uptime mais receptivo (dd: hh: mm: ss) - Módulo Node.js

--- Backend/1 - Node.js/Exercícios/ex002/modules/getOsInfo.js
+++ Backend/1 - Node.js/Exercícios/ex002/modules/getOsInfo.js
@@ -1,10 +1,20 @@
 const os = require('node:os');
 
 function getOsInfo() {
+    const OsUptime = os.uptime();
+
+    const days = Math.floor(OsUptime / (60 * 60 * 24));
+    const hours = Math.floor((OsUptime % (60 * 60 * 24)) / (60 * 60));
+    const minutes = Math.floor((OsUptime % (60 * 60)) / 60);
+    const seconds = Math.floor(OsUptime % 60);
+
+    // Formata com zero à esquerda
+    const pad = (n) => String(n).padStart(2, '0');
+
     const system = os.platform();
     const arch = os.arch();
     const cpu = os.cpus()[0]?.model;
-    const uptime = `${os.uptime()} seconds`;
+    const uptime = `${pad(days)}:${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
     const memoryUse = `${((os.freemem() / os.totalmem()) * 100).toFixed(2)}%`;
 
     const sys = { system, arch, cpu, uptime, memoryUse };
