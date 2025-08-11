65a5f47274ab4671f7000d48ca60be526a0e23e9
Mateus Moura <mateusvcsmoura@gmail.com>
2025-08-11 16:19:05 -0300

    Upload de Exercício 003 | CRUD Posts - Módulo SQL + Node.js (Prisma ORM)

---
+++ Backend/4 - SQL no Node.js e Prisma ORM/Aulas/aula005/src/routes/posts.js
@@ -0,0 +1,67 @@
+const { Router } = require("express");
+const prisma = require("../database");
+
+const postsRouter = Router();
+
+postsRouter.get('/', async (req, res) => {
+    const posts = await prisma.post.findMany({
+        select: {
+            id: true,
+            title: true,
+            content: true,
+            slug: true,
+            published: true,
+            authorId: true
+        }
+    });
+
+    return res.status(200).json(posts);
+});
+
+postsRouter.get('/:id', async (req, res) => {
+    const post = await prisma.post.findUnique({ where: { id: +req.params.id } });
+
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    return res.status(200).json(post);
+});
+
+postsRouter.post('/create', async (req, res) => {
+    const { title, content, authorId } = req.body;
+
+    const baseSlug = title
+        .toLowerCase()
+        .normalize("NFD") // separa acentos das letras
+        .replace(/[\u0300-\u036f]/g, "") // remove acentos
+        .replace(/\s+/g, "-") // troca espaços por '-'
+        .replace(/[^a-z0-9\-]/g, "") // remove caracteres inválidos
+        .replace(/\-+/g, "-") // evita múltiplos '-'
+        .replace(/^\-+|\-+$/g, ""); // remove '-' do início/fim
+
+    const slug = `${baseSlug}-${Date.now()}`;
+
+    const newPost = await prisma.post.create({
+        data: { title, content, slug, authorId }
+    });
+
+    return res.status(201).json(newPost);
+});
+
+postsRouter.put('/:id', async (req, res) => {
+    const { title, content } = req.body;
+
+    const updatedPost = await prisma.post.update({
+        data: { title, content },
+        where: { id: +req.params.id }
+    });
+
+    return res.status(200).json(updatedPost);
+});
+
+postsRouter.delete('/:id', async (req, res) => {
+    const deletedPost = await prisma.post.delete({ where: { id: +req.params.id } });
+
+    return res.status(200).json(deletedPost);
+});
+
+module.exports = postsRouter;
--- Backend/4 - SQL no Node.js e Prisma ORM/Aulas/aula005/src/server.js
+++ Backend/4 - SQL no Node.js e Prisma ORM/Aulas/aula005/src/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const userRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api/users', userRouter);
+app.use('/api/posts', postsRouter);
 
 app.listen(PORT, () => console.log(`server running on http://localhost:${PORT}`));
 
